ae86efc522b7a640585a0ef7960d63a5246cce58
Sean Kelly <shonkelli@gmail.com>
2019-02-05 00:58:04 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
+require 'pry'
 def my_select(collection)
- # your code here!
+ i = 0 
+ output = []
+ while i < collection.length 
+    value = yield(collection[i])
+    if value == true
+      output << collection[i]
+    end
+    i += 1
+    end
+ output
 end
